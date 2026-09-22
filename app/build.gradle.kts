faf52102c004f49f59af0108dba562077e9d9829
Balock Ruthel <46648485+ruthel@users.noreply.github.com>
2026-09-22 01:01:59 -0400

    Compile against Android 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.crabscode.towerfortwo"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.crabscode.towerfortwo"
